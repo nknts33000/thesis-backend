ffaa0a33e58d377475ba3dfb1dde6fbae39bbe80
nikos <nikos@DESKTOP-6P47506>
2024-02-20 02:11:47 +0200

    third

--- src/main/java/com/example/platform/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.platform.repo;
-
-import com.example.platform.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-    User findUserByUsername(String username);
-}
