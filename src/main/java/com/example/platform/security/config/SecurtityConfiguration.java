4c5a9e58a6ccef2e7f708255ca29b6c5e76eb428
N.Dounis <nknts33000@gmail.com>
2024-03-12 19:48:51 +0200

    configuring security

---
+++ src/main/java/com/example/platform/security/config/SecurtityConfiguration.java
@@ -0,0 +1,74 @@
+package com.example.platform.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import com.mojang.authentication.service.UserDetailsServiceImpl;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurtityConfiguration {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("user1Pass"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password(passwordEncoder().encode("user2Pass"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//        Set permissions on endpoints
+                .authorizeHttpRequests(auth -> auth
+//            our public endpoints
+                        .requestMatchers(HttpMethod.POST, "/api/auth/signup/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/authentication-docs/**").permitAll()
+//            our private endpoints
+                        .anyRequest().authenticated())
+                .authenticationManager(authenticationManager)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+}
---
+++ src/main/java/com/example/platform/utils/UserRoles.java
@@ -0,0 +1,7 @@
+package com.example.platform.utils;
+
+public enum UserRoles {
+    USER,
+    ADMIN
+
+}
