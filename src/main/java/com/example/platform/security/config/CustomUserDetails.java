f9652a4267bb2e4e6961473bb9443ef940c09e70
nikos <nikos@DESKTOP-6P47506>
2024-03-14 00:16:18 +0200

    made user details

--- src/main/java/com/example/platform/security/config/CustomUserDetails.java
+++ src/main/java/com/example/platform/security/config/CustomUserDetails.java
@@ -3,12 +3,14 @@ package com.example.platform.security.config;
 import com.example.platform.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CustomUserDetails {
+public class CustomUserDetails implements UserDetails {
     private String email;
     private String password;
     private List<GrantedAuthority> authorities;
@@ -19,5 +21,40 @@ public class CustomUserDetails {
         this.authorities= Arrays.stream(user.getRoles().split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;//email is used as a unique username
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
