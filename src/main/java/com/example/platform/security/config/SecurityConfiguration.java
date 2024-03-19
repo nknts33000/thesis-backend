11deb3eba384910e2b2a060b34f77b4a3fcf69c2
nikos <nikos@DESKTOP-6P47506>
2024-03-20 00:14:04 +0200

    Still configuring security

--- src/main/java/com/example/platform/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/platform/security/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+//
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
 
