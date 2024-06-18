f7a029599df6ab3b96ba51597adb0f9b351f2771
N.Dounis <nknts33000@gmail.com>
2024-06-18 15:27:40 +0300

    latest update 2024-06-18

--- src/main/java/com/example/platform/data/DataLoader.java
+++ src/main/java/com/example/platform/data/DataLoader.java
@@ -36,69 +36,70 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Faker faker = new Faker();
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("user_data.txt"))) {
-            // Create Users
-            for (int i = 0; i < 100; i++) {
-                String firstName = faker.name().firstName();
-                String lastName = faker.name().lastName();
-                String email = faker.internet().emailAddress();
-                String password = faker.internet().password();
-                String location = faker.address().city();
-                //String roles = "USER";
-
-                //User user = new User(email, password, firstName, lastName, location);
-                //user.setRoles(roles);
-//                userRepository.save(user);
-                userService.addUser(
-                        new RegistrationDTO(email,password,firstName,lastName,location)
-                );
-
-                User user=userService.getUser(email);
-
-                // Write user data to file
-                writer.write(String.format("User %d: %s %s, Email: %s, Password: %s, Location: %s", i + 1, firstName, lastName, email, password, location + "\n"));
-
-                // Create Companies for the User
-                int numOfCompanies = faker.number().numberBetween(1, 5);
-                for (int j = 0; j < numOfCompanies; j++) {
-                    String companyName = faker.company().name();
-                    String mission = faker.company().catchPhrase();
-
-                    Company company = new Company(companyName, mission, user);
-                    companyRepository.save(company);
-
-                    // Write company data to file
-                   // writer.write(String.format("  Company %d: %s, Mission: %s%n", j + 1, companyName, mission));
-
-                    // Create Adverts for the Company
-                    int numOfAdverts = faker.number().numberBetween(1, 10);
-                    Set<Advert> adverts = new HashSet<>();
-                    for (int k = 0; k < numOfAdverts; k++) {
-                        String jobTitle = faker.job().title();
-                        String jobSummary = faker.lorem().paragraph();
-                        String jobLocation = faker.address().city();
-                        String contactInformation = faker.internet().emailAddress();
-
-                        Advert advert = new Advert(jobTitle, jobSummary, jobLocation, contactInformation, company);
-                        adverts.add(advert);
-                        advertRepository.save(advert);
-
-                        // Write advert data to file
-                        //writer.write(String.format("    Advert %d: Job Title: %s, Job Summary: %s, Location: %s, Contact Information: %s%n", k + 1, jobTitle, jobSummary, jobLocation, contactInformation));
-                    }
-
-                    company.setAdverts(adverts);
-                    companyRepository.save(company);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (UserNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (UserExistsException e) {
-            throw new RuntimeException(e);
-        }
+//        Faker faker = new Faker();
+//
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter("user_data.txt"))) {
+//            // Create Users
+//            for (int i = 0; i < 100; i++) {
+//                String firstName = faker.name().firstName();
+//                String lastName = faker.name().lastName();
+//                String email = faker.internet().emailAddress();
+//                String password = faker.internet().password();
+//                String location = faker.address().city();
+//                //String roles = "USER";
+//
+//                //User user = new User(email, password, firstName, lastName, location);
+//                //user.setRoles(roles);
+////                userRepository.save(user);
+//                userService.addUser(
+//                        new RegistrationDTO(email,password,firstName,lastName,location)
+//                );
+//
+//                User user=userService.getUser(email);
+//
+//                // Write user data to file
+//                writer.write(String.format("User %d: %s %s, Email: %s, Password: %s, Location: %s", i + 1, firstName, lastName, email, password, location + "\n"));
+//
+//                // Create Companies for the User
+//                int numOfCompanies = faker.number().numberBetween(1, 5);
+//                for (int j = 0; j < numOfCompanies; j++) {
+//                    String companyName = faker.company().name();
+//                    String mission = faker.company().catchPhrase();
+//
+//                    Company company = new Company(companyName, mission, user);
+//                    companyRepository.save(company);
+//
+//                    // Write company data to file
+//                   // writer.write(String.format("  Company %d: %s, Mission: %s%n", j + 1, companyName, mission));
+//
+//                    // Create Adverts for the Company
+//                    int numOfAdverts = faker.number().numberBetween(1, 10);
+//                    Set<Advert> adverts = new HashSet<>();
+//                    for (int k = 0; k < numOfAdverts; k++) {
+//                        String jobTitle = faker.job().title();
+//                        //String jobSummary = faker.lorem().paragraph();
+//                        String jobSummary = faker.job().keySkills() + " required. " + faker.job().position();
+//                        String jobLocation = faker.address().city();
+//                        String contactInformation = faker.internet().emailAddress();
+//
+//                        Advert advert = new Advert(jobTitle, jobSummary, jobLocation, contactInformation, company);
+//                        adverts.add(advert);
+//                        advertRepository.save(advert);
+//
+//                        // Write advert data to file
+//                        //writer.write(String.format("    Advert %d: Job Title: %s, Job Summary: %s, Location: %s, Contact Information: %s%n", k + 1, jobTitle, jobSummary, jobLocation, contactInformation));
+//                    }
+//
+//                    company.setAdverts(adverts);
+//                    companyRepository.save(company);
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (UserNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (UserExistsException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
